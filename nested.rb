1b11ea75f1a028b3a457e66da655ab3259c33ee1
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-29 14:04:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,9 +82,9 @@ def adding_matz
         :languages => ["C"]
       }
     }
-    return programmer_hash[:yukihiro_matsumoto] = {
+  programmer_hash[:yukihiro_matsumoto] = {
       :known_for => "Ruby",
-      :languages => []
+      :languages => ['LISP', 'C']
     }
 
 end
