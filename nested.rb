14c4a0bfe5e581b0b94a8e4abe4a0cbd592b3cb2
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-29 14:08:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -135,6 +135,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][languages].puts
+    programmer_hash[:dennis_ritchie][:languages] << 'Assembly'
+    return programmer_hash
 
 end
